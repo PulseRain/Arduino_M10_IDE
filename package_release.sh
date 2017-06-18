719bd553a7e05aeb973303095cd52f791de33733
PulseRainmaker <pulserain@pulserain.com>
2017-06-18 00:27:45 -0700

    add copyright banner to package_release.sh

--- package_release.sh
+++ package_release.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+###############################################################################
+# Copyright (c) 2017, PulseRain Technology LLC 
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (LGPL) as 
+# published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
 FP51_version=1.3.1
 M10_compiler_version=1.2.0
 M10_upload_version=1.1.0
