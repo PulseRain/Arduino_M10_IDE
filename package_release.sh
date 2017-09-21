49bd4ec3b3ec3b32de8ac40f7657c6c9f7f62757
PulseRainmaker <pulserain@pulserain.com>
2017-09-20 21:30:55 -0700

    add M10_compiler_1.2.3.tar.gz and FP51_1.3.7.tar.gz

--- package_release.sh
+++ package_release.sh
@@ -18,8 +18,8 @@
 #
 ###############################################################################
 
-FP51_version=1.3.6
-M10_compiler_version=1.2.2
+FP51_version=1.3.7
+M10_compiler_version=1.2.3
 M10_upload_version=1.2.0
 
 
